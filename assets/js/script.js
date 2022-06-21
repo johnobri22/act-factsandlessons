4330b3645d47fc5bd2eda738ddacd2fdb244ba11
johnobri22 <johncodeinstitute@gmail.com>
2022-06-21 21:33:44 +0000

    http.server

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayStanQuestion () {
+
+}
+
+function displayNewQuestion () {
+
+}
+
+function displayChekQuestion () {
+
+}
+
+function displayRealQuestion () {
+
+}
