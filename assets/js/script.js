710cebff3dc0237cb02e9d1fb0f2d7df449a0fb6
johnobri22 <johncodeinstitute@gmail.com>
2022-08-10 14:46:14 +0000

    Stan and Chek done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-function stan(){document.getElementById("demo").innerHTML="Stanislavski question";}
+function stan(){document.getElementById("demo").innerHTML=`Let's imagine I have to fulfil a task on stage in the theatre. Then there is something strange because my close friend is in the theatre. Why should someone
+come to see me in the theatre and why am I on stage? This prevents me from
+believing in the task I have been given. I have to explain away all these
+inconsistencies. To do this the student has to imagine in detail all the attendant
+circumstances, he has to imagine the precise history of the given circumstances.
+This imaginary explanation can be very naive, simple or even stupid and in itself
+may not be able to be justified but that does not matter. Our imaginary explanation
+must help us justify the inconsistencies in a given task and, once we have filled in
+the blanks, make us believe that all the contradictory facts are possible.
+I can justify the given task, for example, this way. I have come to the theatre
+with my sick friend because my landlord has managed to throw me out of my
+apartment for his own reasons and because I cannot find a new apartment for the
+moment and so I have been obliged to ask the director of the theatre to allow me
+to stay there for the time being, etc.
+I am on stage because the manager has ordered all the floors in the theatre to
+be polished and has sent me on stage while it is being done, etc.
+My visitor has come first because I have to be here for a while and second
+because he is leaving Moscow today, etc.`;}
+
+function chek(){document.getElementById("demo2").innerHTML=`Let's imagine I have to fulfil a task on stage in the theatre. Then there is something strange because my close friend is in the theatre. Why should someone
+come to see me in the theatre and why am I on stage? This prevents me from
+believing in the task I have been given. I have to explain away all these
+inconsistencies. To do this the student has to imagine in detail all the attendant
+circumstances, he has to imagine the precise history of the given circumstances.
+This imaginary explanation can be very naive, simple or even stupid and in itself
+may not be able to be justified but that does not matter. Our imaginary explanation
+must help us justify the inconsistencies in a given task and, once we have filled in
+the blanks, make us believe that all the contradictory facts are possible.
+I can justify the given task, for example, this way. I have come to the theatre
+with my sick friend because my landlord has managed to throw me out of my
+apartment for his own reasons and because I cannot find a new apartment for the
+moment and so I have been obliged to ask the director of the theatre to allow me
+to stay there for the time being, etc.
+I am on stage because the manager has ordered all the floors in the theatre to
+be polished and has sent me on stage while it is being done, etc.
+My visitor has come first because I have to be here for a while and second
+because he is leaving Moscow today, etc.`;}
