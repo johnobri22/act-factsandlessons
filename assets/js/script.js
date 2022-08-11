86f0c5900436ae7f30743872e0873e45784a5e20
johnobri22 <johncodeinstitute@gmail.com>
2022-08-11 15:33:16 +0000

    Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,5 +19,28 @@ because he is leaving Moscow today, etc.`;}
 
 function chek(){document.getElementById("demo2").innerHTML=`Nephew of Anton Chekhov. Heavily influenced by Schopenhauer, Rudolf Steiner and anthroposophy.`;}
 
-function france(){document.getElementById("demo3").innerHTML=`Nephew of Anton Chekhov. Heavily influenced by Schopenhauer, Rudolf Steiner and anthroposophy.`;}
+function france(){document.getElementById("demo3").innerHTML=`The audience can choose to believe that what it is seeing is not a lie, but the actor must know that it is a lie and find a truth within that lie through his art which must be founded in some kind of reality
+
+Jouvet makes a crucial distinction between the personality actor, the acteur, and the actor who transforms himself into someone else, the comedien. The comédien must examine, test out his subjective feelings in relation to the text, with all its demands, its historical factors, so that what he offers becomes 'objective'. 
+
+Rousseau: 'The character is not the individual that we are but the individual we wish to persuade others that we are'
+
+It is the tensions between self and character, the awareness of playing a fiction that brings a role to life
+
+A man can fulfil a variety of roles and functions as he goes through life: son, brother, nephew, father, husband, lover, bank manager, gambler 
+
+Sometimes there is a conflict between certain aspects of the same life. A man can be an army officer and a Catholic with a conflict between the two. At the centre of any given person, consciously or not, whether he knows it or not, there is a secret.
+
+There is no theme in a play, there is an action and it is for the audience to interpret that action, to see in it whatever it wants.`;}
+
+function real(){document.getElementById("demo4").innerHTML=`The actor must make it his own to such an extent that the thoughts and efforts of his character become his own and are constantly in his mind throughout the performance  
+
+Realism depicts characters and settings as they would actually have existed, while naturalism concentrates on the biological, social and economic aspects
+
+Shchepkin:
+An actor must be able to feel and sympathise with all sorts and classes and behave in ways appropriate to different social groups. • In comedy, an actor should not consciously attempt to be funny, comedy and tragedy both arise from truthful behaviour.   
+
+These principles represent the first genuine, modern statement on acting; there are no references to rhetoric, declamation or rules of gesture
+
+One actor does not cry on stage but by giving, so to speak, a semblance of tears, makes the audience cry.`;}
 
